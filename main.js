439fab916070baf0f58f567cfce13cb0eef37473
skiotto93 <fabio.forenza@hotmail.com>
2022-11-02 17:35:27 +0100

    collegato ciclo for a evento click su play btn

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 "use strict";
-const boardContainer = document.querySelector(".board")
+const boardContainer = document.querySelector(".board");
+const easy = document.querySelector(".easy");
+const normal = document.querySelector(".normal");
+const hard = document.querySelector(".hard");
 
-for ( let i = 1; i <= 100; i++ ) {
-    const boardCell = document.createElement("div");
-    boardCell.innerHTML = i;
-    boardCell.classList.add("board-cell");
-    boardCell.addEventListener('click', function() {
-        boardCell.classList.add("color-add")
-        console.log(this.innerHTML);
-    }       
-    );
-    boardContainer.append(boardCell);
+
+
+const start = document.querySelector(".start-button");
+start.addEventListener('click', function () {
+    for ( let i = 1; i <= 100; i++ ) {
+        const boardCell = document.createElement("div");
+        boardCell.innerHTML = i;
+        boardCell.classList.add("board-cell-100");
+        boardCell.addEventListener('click', function() {
+            boardCell.classList.add("color-add")
+            console.log(this.innerHTML);
+        }       
+        );
+        boardContainer.append(boardCell);
+    };
+    if 
 }
+) 
