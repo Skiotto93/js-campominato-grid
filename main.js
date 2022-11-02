e435e4e6b3bf74da00f998a73338e9704a6e558e
skiotto93 <fabio.forenza@hotmail.com>
2022-11-02 17:45:21 +0100

    Aggiunto if scrauso

--- main.js
+++ main.js
@@ -8,17 +8,39 @@ const hard = document.querySelector(".hard");
 
 const start = document.querySelector(".start-button");
 start.addEventListener('click', function () {
-    for ( let i = 1; i <= 100; i++ ) {
-        const boardCell = document.createElement("div");
-        boardCell.innerHTML = i;
-        boardCell.classList.add("board-cell-100");
-        boardCell.addEventListener('click', function() {
-            boardCell.classList.add("color-add")
-            console.log(this.innerHTML);
-        }       
-        );
-        boardContainer.append(boardCell);
-    };
-    if 
-}
-) 
+    if (easy) {
+        for ( let i = 1; i <= 100; i++ ) {
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i;
+            boardCell.classList.add("board-cell-100");
+            boardCell.addEventListener('click', function() {
+                boardCell.classList.add("color-add")
+                console.log(this.innerHTML);
+            }       
+            );
+            boardContainer.append(boardCell);
+        };
+    } else if (normal) {
+        for ( let i = 1; i <= 81; i++ ) {
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i;
+            boardCell.classList.add("board-cell-81");
+            boardCell.addEventListener('click', function() {
+                boardCell.classList.add("color-add")
+                console.log(this.innerHTML);
+            }       
+            );
+            boardContainer.append(boardCell);
+    }
+    } else if (hard) {
+        for ( let i = 1; i <= 49; i++ ) {
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i;
+            boardCell.classList.add("board-cell-49");
+            boardCell.addEventListener('click', function() {
+                boardCell.classList.add("color-add")
+                console.log(this.innerHTML);
+            }       
+            );
+            boardContainer.append(boardCell);
+    }; 
